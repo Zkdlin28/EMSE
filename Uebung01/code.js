f6d7c4415fb03f7dc1b7714018a723e42ac1083b
Zkdlin28 <evleen18issa@gmail.com>
2025-05-05 16:55:22 +0200

    Experiment angepasst

--- Uebung01/code.js
+++ Uebung01/code.js
@@ -1,82 +1,127 @@
 let SEED = "666";
 Nof1.SET_SEED(SEED);
 
-let experiment_configuration_function = (writer) => { return {
+let experiment_configuration_function = (writer) => {
+    return {
+        experiment_name: "Datentypen-Verständnis",
+        seed: SEED,
 
-    experiment_name: "TestExperiment",
-    seed: SEED,
-
-    introduction_pages: writer.stage_string_pages_commands([
-        writer.convert_string_to_html_string(
-            "Please, just do this experiment only, when you have enough time, are concentrated enough, and motivated enough.\n\nPlease, open the browser in fullscreen mode (probably by pressing [F11])."),
-        writer.convert_string_to_html_string(
-            "In this experiment, you will be asked to manually compute the result of an mathematical term.\n\nDon't worry, the terms are not too complex.")
-    ]),
-
-    pre_run_training_instructions: writer.string_page_command(
-        writer.convert_string_to_html_string(
-            "You entered the training phase."
-        )),
-
-    pre_run_experiment_instructions: writer.string_page_command(
-        writer.convert_string_to_html_string(
-            "You entered the experiment phase.\n\n"
-        )),
-
-    finish_pages: [
-        writer.string_page_command(
+        introduction_pages: writer.stage_string_pages_commands([
             writer.convert_string_to_html_string(
-                "Almost done. Next, the experiment data will be downloaded. Please, send the " +
-                "downloaded file to the experimenter.\n\nAfter sending your email, you can close this window.\n\nMany thanks for participating in the experiment."
+                "Willkommen zum Experiment.\n\nBitte arbeite konzentriert und im Vollbildmodus ([F11])."
+            ),
+            writer.convert_string_to_html_string(
+                "Du wirst gleich 10 Codebeispiele sehen.\n\nGib als Antwort eine Zahl ein und bestätige mit [Enter]."
             )
-        )
-    ],
+        ]),
 
-    layout: [
-        { variable: "Dummy",  treatments: ["X"]},
-    ],
+        pre_run_training_instructions: writer.string_page_command(
+            writer.convert_string_to_html_string("Training abgeschlossen. Jetzt beginnt das Experiment.")
+        ),
 
-    training_configuration: {
-        fixed_treatments:               [
-            ["Dummy", "X"]
+        pre_run_experiment_instructions: writer.string_page_command(
+            writer.convert_string_to_html_string("Bereit? Es folgen 10 Aufgaben.")
+        ),
+
+        finish_pages: [
+            writer.string_page_command(
+                writer.convert_string_to_html_string("Experiment beendet. Vielen Dank für deine Teilnahme!")
+            )
         ],
-        can_be_cancelled: false,
-        can_be_repeated: false
-    },
 
-    repetitions: 10,
+        layout: [
+            { variable: "CodeType", treatments: ["sprechende", "abstrakt"] }
+        ],
 
-    measurement: Nof1.Reaction_time(Nof1.keys(["1", "2", "3", "9"])),
+        training_configuration: {
+            fixed_treatments: [["CodeType", "sprechende"]],
+            can_be_cancelled: false,
+            can_be_repeated: false
+        },
 
-    task_configuration:    (t) => {
+        repetitions: 10,
 
-        let random_int = Nof1.new_random_integer(10);
+        measurement: Nof1.Reaction_time(Nof1.keys(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"])),
 
-        t.do_print_task = () => {
-            writer.clear_stage();
-            writer.print_html_on_stage("<div class='sourcecode'>Hi, this is some source code, enter abc </div>");
-        };
+        task_configuration: (t) => {
+            const examples = [
+                {
+                    code: `let x = 0;\nfor (let i = 0; i < 3; i++) {\n  x++;\n}\nconsole.log(x);`,
+                    answer: "3"
+                },
+                {
+                    code: `let sum = 0;\nfor (let i = 1; i <= 3; i++) {\n  sum += i;\n}\nconsole.log(sum);`,
+                    answer: "6"
+                },
+                {
+                    code: `let v = 5;\nif (v > 3) {\n  v -= 2;\n}\nconsole.log(v);`,
+                    answer: "3"
+                },
+                {
+                    code: `let n = 4;\nif (n % 2 == 0) {\n  n /= 2;\n}\nconsole.log(n);`,
+                    answer: "2"
+                },
+                {
+                    code: `let c = 0;\nfor (let i = 0; i < 5; i++) {\n  if (i % 2 === 0) c++;\n}\nconsole.log(c);`,
+                    answer: "3"
+                },
+                {
+                    code: `let y = 1;\nfor (let i = 0; i < 2; i++) {\n  y += 2;\n}\nconsole.log(y);`,
+                    answer: "5"
+                },
+                {
+                    code: `let a = 7;\nif (a < 10) {\n  a -= 4;\n}\nconsole.log(a);`,
+                    answer: "3"
+                },
+                {
+                    code: `let p = 0;\nfor (let j = 0; j < 4; j++) {\n  p += 1;\n}\nconsole.log(p);`,
+                    answer: "4"
+                },
+                {
+                    code: `let b = 6;\nif (b % 3 == 0) {\n  b = b / 2;\n}\nconsole.log(b);`,
+                    answer: "3"
+                },
+                {
+                    code: `let v = 3;\nif (v > 2) {\n  v += 2;\n}\nconsole.log(v);`,
+                    answer: "5"
+                }
+            ];
 
-        t.expected_answer = "abc";
+            if (typeof window.exampleCounter === "undefined") {
+                window.exampleCounter = 0;
+            }
 
-        t.accepts_answer_function = (given_answer) => {
-            return true;
-        };
+            const current = examples[window.exampleCounter % examples.length];
+            window.exampleCounter++;
 
-        t.do_print_error_message = (given_answer) => {
-            writer.clear_stage();
-            writer.clear_error();
-            writer.print_html_on_error("<h1>Invalid answer: " + given_answer + "");
-        };
+            t.expected_answer = current.answer;
 
-        t.do_print_after_task_information = () => {
-            writer.clear_error();
-            writer.print_string_on_stage(writer.convert_string_to_html_string(
-                "Correct.\n\n" +
-                "In case, you feel not concentrated enough, make a short break.\n\n" +
-                "Press [Enter] to go on. "));
+            t.do_print_task = () => {
+                writer.clear_stage();
+                writer.print_html_on_stage(
+                    `<div class='sourcecode'><pre>${current.code}</pre></div>`
+                );
+            };
+
+            t.accepts_answer_function = (given_answer) => {
+                t.last_answer = given_answer.trim();
+                return true;
+            };
+
+            t.do_print_error_message = (given_answer) => {
+                writer.clear_stage();
+                writer.clear_error();
+                writer.print_html_on_error("<h1>Ungültige Eingabe: " + given_answer + "</h1>");
+            };
+
+            t.do_print_after_task_information = () => {
+                writer.clear_error();
+                writer.print_string_on_stage(writer.convert_string_to_html_string(
+                    "Drücke [Enter], um weiterzumachen."
+                ));
+            };
         }
-    }
-}};
+    };
+};
 
 Nof1.BROWSER_EXPERIMENT(experiment_configuration_function);
